6e619d2fbdfac364d22ef20ba742f57c3bd28c2b
Trenton Scott <InsaneGriot@Outlook.com>
2025-11-11 08:58:56 -0500

    CreateWeaponsWindow added

--- ArmorWindow.xaml.cs
+++ ArmorWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Star_Reverie_Inventory_Manager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ArmorWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ArmorWindow : Window
     {
-        public MainWindow()
+        public ArmorWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            CreateWeaponsWindow createWeaponsWindow = new();
-            createWeaponsWindow.ShowDialog();
         }
     }
 }
--- CreateWeaponsWindow.xaml.cs
+++ CreateWeaponsWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,25 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Star_Reverie_Inventory_Manager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CreateWeaponsWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CreateWeaponsWindow : Window
     {
-        public MainWindow()
+        public CreateWeaponsWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            CreateWeaponsWindow createWeaponsWindow = new();
-            createWeaponsWindow.ShowDialog();
+            //Save weapon
+            Close();
         }
     }
 }
