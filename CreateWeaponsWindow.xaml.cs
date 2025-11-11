b2cf52d50a11884b0d5d4be52147249d2329a189
Trenton Scott <InsaneGriot@Outlook.com>
2025-11-11 09:53:38 -0500

    Adding dll file and other things

--- CreateWeaponsWindow.xaml.cs
+++ CreateWeaponsWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StarReverieCore.Equipment;
+using StarReverieCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,14 +21,26 @@ namespace Star_Reverie_Inventory_Manager
     /// </summary>
     public partial class CreateWeaponsWindow : Window
     {
+        public Array DamageTypes { get; } = Enum.GetValues(typeof(DamageType));
+        public Array WeaponTypes { get; } = Enum.GetValues(typeof(WeaponType));
+
+        public DamageType SelectedDamageType { get; set; } = DamageType.Piercing;
+        public WeaponType SelectedWeaponType { get; set; } = WeaponType.RangedPhysical;
         public CreateWeaponsWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             //Save weapon
+            //WeaponModel weaponModel = new()
+            //{
+            //    Name = nameTextBox.Text,
+            //    DamageType = SelectedDamageType,
+            //    WeaponType = SelectedWeaponType,
+            //};
             Close();
         }
     }
