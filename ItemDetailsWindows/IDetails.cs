8e528df89356a2a51d2670c42f708232006dbf87
Trenton Scott <InsaneGriot@Outlook.com>
2025-12-03 23:47:27 -0500

    Adding technique menus and things

---
+++ ItemDetailsWindows/IDetails.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// 	IDetails.cs
+// 	Author: Trenton Scott 
+// 	Copyright (c) Centuras. All rights reserved.
+//  -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Star_Reverie_Inventory_Manager.ItemDetailsWindows
+{
+    public interface IDetails
+    {
+        void UpdateButton_Click(object sender, RoutedEventArgs e);
+        void DeleteButton_Click(object sender, RoutedEventArgs e);
+        void ExitButton_Click(object sender, RoutedEventArgs e);
+    }
+}
--- obj/Debug/net10.0-windows7.0/MainWindow.g.i.cs
+++ obj/Debug/net10.0-windows7.0/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace Star_Reverie_Inventory_Manager {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Star Reverie Inventory Manager;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Star Reverie Inventory Manager;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
