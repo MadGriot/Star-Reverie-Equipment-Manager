fc609e79728cfbf83f1fa3b5301816a30ba27222
Trenton Scott <InsaneGriot@Outlook.com>
2025-12-06 23:59:13 -0500

    Strategy Factory?

---
+++ StrategyFactory.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// 	StrategyFactory.cs
+// 	Author: Trenton Scott 
+// 	Copyright (c) Centuras. All rights reserved.
+//  -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Star_Reverie_Inventory_Manager
+{
+    internal class StrategyFactory
+    {
+    }
+}
