e0fd6d365e3359f70c34b8e5adb095da7ccdbd39
Trenton Scott <InsaneGriot@Outlook.com>
2025-12-29 16:52:01 -0500

    More Daily Characters

--- CharacterManager/DisplayPlatoonSquadsWindow.xaml.cs
+++ CharacterManager/DisplayPlatoonSquadsWindow.xaml.cs
@@ -4,19 +4,9 @@
 // 	Copyright (c) Centuras. All rights reserved.
 //  -----------------------------------------------------------------------
 
-using Star_Reverie_Inventory_Manager.ItemDetailsWindows;
 using StarReverieCore.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Star_Reverie_Inventory_Manager.CharacterManager
 {
